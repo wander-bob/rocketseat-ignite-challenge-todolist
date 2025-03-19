7d01e5ff16b8e558a242f65a66211f5a6b9c7ee9
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-03-19 11:25:44 -0300

    feat: Creating layout structure and base style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { Header } from './components/Header';
+
+import styles from './App.module.css';
+
 export function App() {
   return (
-    <>
-      <div className="teste"></div>
-    </>
+    <div className={styles.wrapper}>
+      <Header />
+      <main>
+        <div className="add-task-input">
+          <input type="text" placeholder="Adicione uma nova tarefa" />
+        </div>
+        <button>criar</button>
+
+        <div className="task-board">
+          <div className="counter">
+            <div className="close-tasks">
+              Title
+              <p>counter</p>
+            </div>
+            <div className="opened-tasks">
+              Title
+              <p>counter</p>
+            </div>
+            <div className="task-list">
+              <div className="task">
+                <button>state</button>
+                content
+                <button>delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
